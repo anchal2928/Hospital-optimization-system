d028e166dcabd28cd0db30a4787d04c4e17c1d1f
Anchal Chauhan <anchalchauhan671@gmail.com>
2024-09-15 13:07:38 +0530

    Add files via upload

---
+++ Book_an_appointmentJavascript.js
@@ -0,0 +1,22 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+function openRegister() {
+  document.getElementById("register").style.width = "100%";
+}
+
+function closeRegister() {
+  document.getElementById("register").style.width = "0%";
+}
+
+function closeicon1() {
+  document.getElementById("icon1").style.width = "0%";
+}
+function closeicon2() {
+  document.getElementById("icon2").style.width = "0%";
+}
---
+++ java.js
@@ -0,0 +1,34 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+function openRegister() {
+  document.getElementById("register").style.width = "100%";
+}
+
+function closeRegister() {
+  document.getElementById("register").style.width = "0%";
+}
+
+function closeicon1() {
+  document.getElementById("icon1").style.width = "0%";
+}
+function closeicon2() {
+  document.getElementById("icon2").style.width = "0%";
+}
+
+$('#Parent_aboutUS').scroll(function() { 
+  $('#FixedDiv').css('top', $(this).scrollTop());
+});
+
+
+
+
+
+
+
+
